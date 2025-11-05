1425f83ae32f3c5f5b03ac9a82b4e12d2f508389
adrian galan pacheco <agalanpa@uoc.edu>
2025-11-05 11:12:16 +0100

    PEC1

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,28 @@
+import * as basicLightbox from "basiclightbox";
+import "basicLightbox/dist/basiclightbox.min.css";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const currentPagePath = window.location.pathname;
+    const navLinks = document.querySelectorAll(".header__sidebar a");
+
+    navLinks.forEach((link) => {
+        const linkPath = link.getAttribute("href");
+
+        if (linkPath && currentPagePath.endsWith(linkPath)) {
+            link.classList.add("is-active");
+        }
+    });
+
+    const imagesToLightbox = document.querySelectorAll(".details img");
+    if (imagesToLightbox.length > 0) {
+        imagesToLightbox.forEach((image) => {
+            console.log(image);
+            image.addEventListener("click", (event) => {
+                event.preventDefault();
+                basicLightbox
+                    .create(`<img src="${image.src}" alt="${image.alt}">`)
+                    .show();
+            });
+        });
+    }
+});
